f490ea70d4f1ce757d96f55ef24e16a7a94c4f97
Answerer-ryo <answerer-ryo@Answerer-ryo-no-MacBook-Pro.local>
2014-07-09 11:19:59 +0900

    Initial Commit

---
+++ newLiFE/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  newLiFE
+//
+//  Created by Answerer-ryo on 2014/07/09.
+//  Copyright (c) 2014年 Answerer-ryo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ newLiFE/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  newLiFE
+//
+//  Created by Answerer-ryo on 2014/07/09.
+//  Copyright (c) 2014年 Answerer-ryo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
